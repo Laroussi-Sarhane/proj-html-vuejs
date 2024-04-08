167412283fc94259c97cc9adcd86ee1b72741e43
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-04-08 14:55:16 +0200

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+
 export default {
+  components:{
+    Header,
+    Main,
+    Footer
+  }
 
 }
 </script>
 <template>
+  
+  <Header/>
+  <Main/>
+  <Footer/>
+
   
 </template>
 
