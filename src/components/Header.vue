b523f437964694174c97fb88f3725a6a2e490876
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-04-08 18:07:59 +0200

    add jumbo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,10 +19,7 @@ export default {
           title: 'JOURNAL',
           link: '#'
         },
-        {
-          title: 'DONATE',
-          link: '#'
-        }
+       
       ]
     }
   }
@@ -32,21 +29,49 @@ export default {
 <template>
 
   <header>
+    <!-- top header -->
 
-    <div class=" container d-flex justify-content-between  ">
+    <div class=" container d-flex justify-content-between pt-3">
    
      <div class=" pt-2">
        <img src="../assets/img/avada-charity-logo.png" alt="">
      </div>
    
      <div>
-       <ul class=" d-flex list-unstyled">
+       <ul class=" d-flex list-unstyled  ">
          <li v-for="(item, index) in Menu " :key="index" class=" p-2 ">
           <a :href="item.link">{{ item.title }}</a>
         </li>
+        <li  class=" my_botton p-2 ">
+          <a href="">DONATE</a>
+        </li>
        </ul>
      </div>
-   
+    </div>
+    <!-- fine top header -->
+    <div class=" d-flex flex-column  justify-content-center align-content-center ">
+
+     <div class="my_paragraf cdeb d-flex  justify-content-center ">
+       <h1>MAKE A DIFFERENCE</h1>
+     </div>
+
+     <div class=" cdeb d-flex justify-content-center align-content-center ">
+      <p>as long as poverty, injustice & inequality persist, none of us can truly rest </p>
+     </div>
+
+     <div class="cdeb d-flex justify-content-center align-content-center  ">
+      <div class="my_bottone">
+        <button type="button" class="btn btn-outline-light">Light</button>
+      </div>
+
+      <div class="my_secbottone">
+        <button type="button" class="btn btn-outline-warning">Warning</button>
+      </div>
+
+     </div>
+
+     
+
     </div>
   </header>
 
@@ -56,15 +81,43 @@ export default {
 
 <style scoped>
 header{
-  min-height: 550px;
+  min-height: 600px;
   background-color: grey;
+  background-image: url(../assets/img/home-page-slider.jpg);
+  background-size: cover;
+  width: 100%;
 }
 a{
   color: white;
   padding-top: 10px;
+  text-decoration: none;
+}
+a:hover{
+  text-decoration: underline;
+  color: #FCD66C;
 }
 img{
-  width: 150px;
+  width: 250px;
+}
+.my_botton{
+  background-color: #FCD66C;
+  color: black;
+}
+.my_botton.a:hover{
+  color: black;
+}
+.my_paragraf{
+  
+  margin-top: 150px;
+}
+.cdeb{
+  color: white
+}
+.my_bottone{
+  padding-right: 5px;
+}
+.my_secbottone{
+  padding-left: 5px;
 }
 
 </style>
