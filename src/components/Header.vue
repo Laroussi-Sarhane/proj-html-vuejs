f8458bd6757585d6e3307215928cc6353f92ecf0
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-04-08 15:44:36 +0200

    add changes header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,13 +5,39 @@ export default {
 </script>
 <template>
 
- <div>
-  <h1>header</h1>
- </div>
+  <header>
+
+    <div class=" container d-flex justify-content-between  ">
+   
+     <div class=" pt-2">
+       logo
+     </div>
+   
+     <div>
+       <ul class=" d-flex list-unstyled">
+         <li class=" p-2 "><a href="#">home</a></li>
+         <li class=" p-2 "><a href="#">MISSION</a></li>
+         <li class=" p-2 "><a href="#">CAUSES</a></li>
+         <li class=" p-2 "><a href="#">JOURNAL</a></li>
+         <li class=" p-2 "><a href="#">DONATE</a></li>
+       </ul>
+     </div>
+   
+    </div>
+  </header>
+
 
 </template>
 
 
 <style scoped>
+header{
+  min-height: 550px;
+  background-color: grey;
+}
+a{
+  color: white;
+  padding-top: 10px;
+}
 
 </style>
