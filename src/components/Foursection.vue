c7043bcbdc71bc6874ff8227edfb2d6f04576482
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-04-09 16:23:13 +0200

    add section

--- src/components/Foursection.vue
+++ src/components/Foursection.vue
@@ -10,15 +10,117 @@ export default {
   <div class="my_p d-flex justify-content-center col-12  text-center flex-column ">
 
 <div>
-  <h4 class="my_title">RECENT CAUSES</h4>
+  <h4 class="my_title">LATEST ARTICLES</h4>
 </div>
 
 <div class="my_cont d-flex justify-content-center "></div>
 
 <div class=" text-center py-3">
-  <p>we run projects in over 30 countries in 5 continents</p>
-  
+  <p>Together we can make all the difference</p>
 </div>
+
+</div>
+
+<!-- cards  -->
+
+<div class="container d-flex justify-content-center mt-4   ">
+  <div class=" flex-column col-6 ">
+
+    <div class="my_big_box  ">
+      <img src="../assets/img/photo-1444213007800-cff19e1677ac-600x450.jpg" alt="">
+
+      <div class=" flex-column  mt-4  ">
+        <div class=" mt-2 ">
+          <h4 class="my_color p-3 ">Understandig human complexities</h4>
+        </div>
+  
+        <div class=" my_large mt-3 px-3   ">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed nobis dolores sint, voluptate ad exercitationem nam </p>
+        </div>
+  
+      </div>
+
+    </div>
+
+    
+  </div>
+
+  <div class="my_box  ">
+    <div class=" d-flex ">
+
+      <div class="my_img_small">
+        <img src="../assets/img/photo-1444664597500-035db93e2323-177x142.jpg" alt="">
+      </div>
+
+      <div class=" my_text_small flex-column ">
+        <div>
+          <h5 class=" my_color">Understandig human complexities</h5>
+        </div>
+
+        <div class="my_font">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,  </p>
+        </div>
+      </div>
+
+    </div>
+
+    <!--  -->
+        <div class=" d-flex ">
+
+    <div class="my_img_small">
+      <img src="../assets/img/photo-1447430617419-95715602278e-177x142.jpg" alt="">
+    </div>
+
+    <div class=" my_text_small flex-column ">
+      <div>
+        <h5 class=" my_color">Understandig human complexities</h5>
+      </div>
+
+      <div class="my_font">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,  </p>
+      </div>
+    </div>
+
+    </div>
+    <!--  -->
+        <div class=" d-flex ">
+
+    <div class="my_img_small">
+      <img src="../assets/img/photo-1460230525622-630fe3294cd7-177x142.jpg" alt="">
+    </div>
+
+    <div class=" my_text_small flex-column ">
+      <div>
+        <h5 class=" my_color">Understandig human complexities</h5>
+      </div>
+
+      <div class="my_font">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,  </p>
+      </div>
+    </div>
+
+    </div>
+    <!--  -->
+      <div class=" d-flex ">
+
+  <div class="my_img_small">
+    <img src="../assets/img/photo-1460600421604-5e138c208b9c-177x142.jpg" alt="">
+  </div>
+
+  <div class=" my_text_small flex-column ">
+    <div>
+      <h5 class=" my_color">Understandig human complexities</h5>
+    </div>
+
+    <div class="my_font">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,  </p>
+    </div>
+  </div>
+
+  </div>
+
+
+  </div>
 
 </div>
 
@@ -29,8 +131,25 @@ export default {
 
 <style scoped>
 section{
-  min-height: 600px;
+  min-height: 900px;
 
+}
+.my_color{
+  color: #FCD66C;
+  margin-bottom: 20px;
+}
+.my_box{
+  height: 600px;
+  width: 450px;
+  
+}
+.my_large{
+  max-width: 500px;
+}
+.my_big_box{
+  height: 720px;
+  width: 600px;
+  border: 2px solid gray;
 }
 .my_margin{
   margin-top: 50px;
@@ -41,16 +160,27 @@ section{
   margin-left: 46%;
 }
 .my_title{
-  margin-top: 90px;
   color: black;
   font-size: 1.3rem;
 }
-.my_box{
-  width: 200px;
-  height: 150px;
-  margin: 30px;
+.my_img_small{
+  width: 177px;
+  height: 142px;
+  margin-bottom: 50px;
   background-color: #FCD66C;
 }
+.my_text_small{
+  margin-left:10px ;
+  padding: 0px 10px;
+}
+.my_font{
+  font-size: 12px;
+}
+.my_font_title{
+  font-weight: 200;
+  font-size: 17px;
+}
+
 
 
 </style>
